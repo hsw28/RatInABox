fab27a39eb6f7427277287c531fcfca16fd6ef60
hsw28 <holdbackaglacier@yahoo.com>
2023-12-28 18:57:23 -0600

    slurm errors

--- HSW/additive_model/SLURM_additive.sh
+++ HSW/additive_model/SLURM_additive.sh
@@ -17,4 +17,4 @@ export PYTHONPATH="${PYTHONPATH}:/home/hsw967/Programming/RatInABox"
 export PYTHONPATH="${PYTHONPATH}:/home/hsw967/Programming/Hannahs-CEBRAs"
 
 
-python /home/hsw967/Programming/RatInABox/HSW/additive_model/main2.py --balance_values 1 --balance_dist additive --responsive_values 0, .2, .4, .6, .8, 1 --responsive_type fixed --percent_place_cells .4, .6, .8, 1 --num_iters 12 --optional_param work
+python /home/hsw967/Programming/RatInABox/HSW/additive_model/main2.py --balance_values 1 --balance_dist additive --responsive_values 0,.2,.4,.6,.8,1 --responsive_type fixed --percent_place_cells .4,.6,.8,1 --num_iters 12 --optional_param work
