fe849e61c9d21fccd80370dd8ea998d19f968f92
hsw28 <holdbackaglacier@yahoo.com>
2023-12-31 13:02:54 -0600

    cries in slurm

--- HSW/additive_model/SLURM_additive.sh
+++ HSW/additive_model/SLURM_additive.sh
@@ -5,9 +5,11 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mem=16GB
-#SBATCH --time=1:00:00
+#SBATCH --time=0:10:00
 #SBATCH --output=SLURM_errors.out ## standard out and standard error goes to this file
 
+
+printenv
 
 module purge
 eval "$(conda shell.bash hook)"
@@ -16,6 +18,7 @@ source activate ratinabox
 export PYTHONPATH="${PYTHONPATH}:/home/hsw967/Programming/RatInABox"
 export PYTHONPATH="${PYTHONPATH}:/home/hsw967/Programming/Hannahs-CEBRAs"
 
+printenv
 
 #python /home/hsw967/Programming/RatInABox/HSW/additive_model/main2.py --balance_values 1 --balance_dist additive --responsive_values 0,.2,.4,.6,.8,1 --responsive_type fixed --percent_place_cells .4,.6,.8,1 --num_iters 1 --optional_param work
 python /home/hsw967/Programming/RatInABox/HSW/additive_model/test.py
