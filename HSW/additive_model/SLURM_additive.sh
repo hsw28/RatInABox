c13e3d61f0e4bb5466abd811d917628979f8e630
hsw28 <holdbackaglacier@yahoo.com>
2024-01-01 15:03:29 -0600

    slurm updates :)

--- HSW/additive_model/SLURM_additive.sh
+++ HSW/additive_model/SLURM_additive.sh
@@ -4,8 +4,8 @@
 #SBATCH --gres=gpu:a100:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --mem=243GB
-#SBATCH --time=48:00:00
+#SBATCH --mem=180GB
+#SBATCH --time=22:00:00
 #SBATCH --output=SLURM_errors.out ## standard out and standard error goes to this file
 
 
@@ -20,5 +20,5 @@ export PYTHONPATH="${PYTHONPATH}:/home/hsw967/Programming/Hannahs-CEBRAs"
 
 
 #python /home/hsw967/Programming/RatInABox/HSW/additive_model/main2.py --balance_values 1 --balance_dist additive --responsive_values .5 --responsive_type fixed --percent_place_cells .5 --num_iters 1 --optional_param work
-python /home/hsw967/Programming/RatInABox/HSW/additive_model/main2.py --balance_values 1 --balance_dist additive --responsive_values 0,.2,.4,.6,.8,1 --responsive_type fixed --percent_place_cells .4,.6,.8,1 --num_iters 5 --optional_param work
+python /home/hsw967/Programming/RatInABox/HSW/additive_model/main2.py --balance_values 1 --balance_dist additive --responsive_values 0,.2,.4,.6,.8,1 --responsive_type fixed --percent_place_cells .4,.6,.8,1 --num_iters 6 --optional_param work
 #python /home/hsw967/Programming/RatInABox/HSW/additive_model/test.py
